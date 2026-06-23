a824c6c66c676405601490a8cf81474455bc2377
Satyam Kumar <satyam8804378323@gmail.com>
2026-06-23 20:04:47 +0530

    pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: false, // use your own public/manifest.json
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
