34b14e3e2d132f504d263f0a3f8b3a86a0fba2e4
Satyam Kumar <satyam8804378323@gmail.com>
2026-06-23 21:38:59 +0530

    fix userid storing issue

--- backend/src/controllers/url.controller.js
+++ backend/src/controllers/url.controller.js
@@ -35,7 +35,7 @@ export const shortenUrl = async (req, res) => {
     await Url.create({
       code,
       longUrl: url,
-      userId: req.user?.id || null,
+      userId: req.user?.id,
       expiresAt,
     });
 
